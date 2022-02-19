f9ed825dd0409e1846fa8a8c77839784807b1982
Kapralov <kapralov.ae@bk.ru>
2022-02-19 15:39:20 +0300

    Добавил вспомогательные функции в main.js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getRandomInt (from, to) {
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  if (from <0 || to <0) {
+    return 0;
+  }
+  if (from > to) {
+    const memory = from;
+    from = to;
+    to = memory;
+  }
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+getRandomInt();
+
+function compareStringLingth (string, maxLingth) {
+  if (string.length > maxLingth) {
+    return false;
+  }
+
+  return true;
+}
+compareStringLingth ();
