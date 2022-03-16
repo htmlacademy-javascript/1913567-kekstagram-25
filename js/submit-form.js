371aff52087f7fff7c504bc10f56e72404264cf6
Kapralov <kapralov.ae@bk.ru>
2022-03-17 00:17:11 +0300

    add pristine

--- js/submit-form.js
+++ js/submit-form.js
@@ -24,7 +24,6 @@ uploadInput.addEventListener('change', () => {
   document.addEventListener('keydown', pressEsc);
 });
 
-
 function closeUploadModal (evt) {
   evt.preventDefault();
   body.classList.remove('modal-open');
@@ -39,3 +38,56 @@ function pressEsc(evt) {
     closeUploadModal(evt);
   }
 }
+
+const regular = /^#[A-Za-zА-Яа-яЁё0-9]{2,20}$/;
+const form = document.querySelector('#upload-select-image');
+const inputHashtag = form.querySelector('.text__hashtags');
+
+
+const pristine = new Pristine(form);
+
+function onChangeInputHashtag () {
+  inputHashtag.addEventListener('change', () => {
+    const hashtagArray = inputHashtag.value.split(' ');
+    hashtagArray.forEach((element) => {
+      console.log(hashtagArray);
+      let isTrue;
+      console.log('ne validno');
+      isTrue = false;
+      if (regular.test(element)) {
+        console.log('validno');
+        isTrue = true;
+      }
+      return isTrue;
+    });
+  });
+}
+
+// function validateHashtag (){
+
+// }
+pristine.addValidator(inputHashtag, onChangeInputHashtag, 'Что-то не так', 2, false);
+form.addEventListener('submit', valid);
+
+function valid (evt) {
+  evt.preventDefault();
+  pristine.validate();
+  console.log(pristine.validate());
+}
+
+// classTo: 'form__item', // Элемент, на который будут добавляться классы
+//   errorClass: 'form__item--invalid', // Класс, обозначающий невалидное поле
+//   successClass: 'form__item--valid', // Класс, обозначающий валидное поле
+//   errorTextParent: 'form__item', // Элемент, куда будет выводиться текст с ошибкой
+//   errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+//   errorTextClass: 'form__error' // Класс для элемента с текстом ошибки
+// const hashtagConfig = {
+//   classTo: 'text__description',
+//   errorClass: 'form__item--invalid',
+//   successClass: 'form__item--valid',
+//   errorTextParent: 'form__item', // Элемент, куда будет выводиться текст с ошибкой
+//   errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+//   errorTextClass: 'form__error' // Класс для элемента с текстом ошибки
+// };
+
+
